8d50598c8678d96a386a9c5979a35a358e19314f
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-07-27 14:38:12 -0300

    Novos exercícios

---
+++ Modulo05/exercicio-13.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 14;
+$b = 2;
+$c = 3;
+
+echo $a % $b;
+echo "<br>";
+echo $a % $c;
+echo "<br>";
+
+
+
+
+
+
---
+++ Modulo05/exercicio-14.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$nome = "Anderson";
+$idade = "22 anos";
+$linguagem = "PHP";
+
+echo "Olá! Meu nome é ".$nome.", tenho ".$idade." e estou aprendendo:".$linguagem."!";
+
+
+
