79e05ed74ebd57ce2fe4107dd609810a42f18d9b
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-11-16 14:07:29 -0300

    exercicio-19

---
+++ Modulo05/exercicio-19.php
@@ -0,0 +1,30 @@
+<?php
+#OPERADORES DE CAST (INT, BOOLEAN, STRING, FLOAT)
+$a = (int)"testando";
+echo $a."<br>";
+$b = (int)"asd";
+echo $b."<br>";
+
+$c = (int)12.9;
+echo $c."<br>";
+
+$d = (int)5;
+echo $d."<br>";
+
+$e = (int)true;
+echo $e."<br>";
+
+$f = (int)false;
+echo $f."<br>";
+
+$g = (int)[1,2,3];
+echo $g."<br>";
+
+
+$h = (int)[1, 2, 3, 4, 5, 6, 7];
+echo $h."<br>";
+
+
+
+
+
