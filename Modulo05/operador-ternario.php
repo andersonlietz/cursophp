a9a018ff53dbb5772e0decad03379ea9bc9e4fd4
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-11-16 17:46:33 -0300

    Conclusão da sessão 5

---
+++ Modulo05/operador-ternario.php
@@ -0,0 +1,12 @@
+<?php
+//true 
+
+echo 20>10 ? "Deu true <br>" : "Deu false <br>";
+
+//false
+
+echo 20<10 ? "Deu true <br>" : "Deu false <br>";
+
+
+
+
---
+++ Modulo05/operadores-de-atribuição.php
@@ -0,0 +1,19 @@
+<?php
+#OPERADORES DE ATRIBUIÇÃO
+
+//+=
+$a = 0;
+$a +=10;    //$a = $a + 10
+
+echo $a."<br>";
+
+//-=
+$b = 0;
+$b -= 5;//$b = $b -5
+echo $b."<br>";
+
+//*=
+$c = 0;
+$c *= 5;//$c = $c *5
+echo $c."<br>";
+
