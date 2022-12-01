69e0cddd4f2b5db7be83362e1283a57a0319ed48
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-12-01 16:36:46 -0300

    Exercícios 21 e 22

---
+++ Modulo06/exercicio-22.php
@@ -0,0 +1,30 @@
+<?php
+
+$idade1 = 16;
+$idade2 = 18;
+$idade3 = 26;
+
+$maioridade = 18;
+
+$msg = "Você é maior de idade!";
+
+if($idade1 >= $maioridade){
+
+    echo $msg;
+}
+
+if($idade2 >= $maioridade){
+
+    echo $msg;
+}
+
+if($idade3 >= $maioridade){
+
+    echo $msg;
+}
+
+
+
+
+
+
