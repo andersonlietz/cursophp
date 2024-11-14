41f553d61119ce28d237f16bb1d567353cfe4dda
Anderson Lietz <andersonrenatolietztj@gmail.com>
2024-11-14 09:57:47 -0300

    foreach em arrays

---
+++ Modulo11/foreach-em-arrays.php
@@ -0,0 +1,34 @@
+<?php 
+
+
+$anderson = [
+    'nome' => 'Anderson',
+    'idade'=> 24,
+    'profissão'=> 'Programador'
+];
+
+
+$alexia = [
+    'nome' => 'Alexia',
+    'idade'=> 30,
+    'profissão'=> 'Programador'
+];
+
+$robson = [
+    'nome' => 'Robson',
+    'idade'=> 22,
+    'profissão'=> 'Pedreiro'
+];
+
+foreach($matheus as $carac => $value){
+
+    echo "$carac =>$value<br>";
+}
+
+
+
+
+
+
+
+
